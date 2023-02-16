65e9296647b9d6122765666332f42fc16688d6ec
Polina <polinazholudeva511256@gmail.com>
2023-02-16 20:22:44 +0300

    feat:task1 to homework6

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine($"чисел > 0 : {count (InitArray())}");
+
+int [] InitArray()
+{
+    Console.WriteLine("Введите числа через запятую");
+    return Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+}
+
+int count (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+           count++;
+        }
+    }
+    return count;
+}
